6c09c147b18c2781bc6792bff5763965a0a8845b
slawo <slawekpettke@gmail.com>
2022-08-30 10:35:53 +0200

    Singleton

--- src/main/java/singletonExpl/singleton/GameEngine.java
+++ src/main/java/singletonExpl/singleton/GameEngine.java
@@ -1,4 +1,14 @@
 package singletonExpl.singleton;
 
 public class GameEngine {
+
+    private int hp = 100;
+    private String charName = "gracz";
+    public void run(){
+        while(true){
+
+            //input from gamer
+            //changing game status
+        }
+    }
 }
