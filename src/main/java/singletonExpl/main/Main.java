785c82bf10a40c8275ec4e278e047ed9eea61b02
slawo <slawekpettke@gmail.com>
2022-08-30 11:47:07 +0200

    Singleton

--- src/main/java/singletonExpl/main/Main.java
+++ src/main/java/singletonExpl/main/Main.java
@@ -1,4 +1,13 @@
 package singletonExpl.main;
 
+import singletonExpl.singleton.GameEngine;
+
 public class Main {
+    public static void main(String[] args) {
+
+        GameEngine engine =GameEngine.getInstance();
+        GameEngine engine2 =GameEngine.getInstance();
+
+        System.out.println(engine == engine2);
+    }
 }
--- src/main/java/singletonExpl/singleton/GameEngine.java
+++ src/main/java/singletonExpl/singleton/GameEngine.java
@@ -4,11 +4,29 @@ public class GameEngine {
 
     private int hp = 100;
     private String charName = "gracz";
-    public void run(){
-        while(true){
+
+    //tworzymy pole priv klasy o nazwie instance -> 2
+    private static GameEngine instance;
+
+    //tworzymy priv konstruktor dla klasy GameEngine -> 1
+    private GameEngine() {
+
+    }
+
+    public void run() {
+        while (true) {
 
             //input from gamer
             //changing game status
+            //graphic rendering
         }
+    }
+
+    //metoda publiczna zwracajaca nam instancje klasy -> 3
+    public static GameEngine getInstance() {
+        if (instance == null) {
+            instance = new GameEngine();
+        }
+        return instance;
     }
 }
