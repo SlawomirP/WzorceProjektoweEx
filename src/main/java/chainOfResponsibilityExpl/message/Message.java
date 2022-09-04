ddb775074b0ac186361c380b80f3c99d1294c2bd
slawo <slawekpettke@gmail.com>
2022-09-04 13:42:53 +0200

    Pattern chain of responsibility

--- src/main/java/chainOfResponsibilityExpl/message/Message.java
+++ src/main/java/chainOfResponsibilityExpl/message/Message.java
@@ -1,4 +1,14 @@
 package chainOfResponsibilityExpl.message;
 
 public class Message {
+
+    private String content;
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
