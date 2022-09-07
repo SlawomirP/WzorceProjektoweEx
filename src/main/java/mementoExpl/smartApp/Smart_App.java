e66d96031c8b34a1adc2a0743206d810b890539b
slawo <slawekpettke@gmail.com>
2022-09-07 17:11:28 +0200

    MementoPattern

--- src/main/java/mementoExpl/smartApp/Smart_App.java
+++ src/main/java/mementoExpl/smartApp/Smart_App.java
@@ -4,16 +4,17 @@ public class Smart_App {
 
     private Double version;
 
-    public void changeVersion(Double version){
+    public void changeVersion(Double version) {
         this.version = version;
         System.out.println("New version: " + this.version);
     }
 
     //metody do zapisywania i odczytywania pamiatek, rowniez publiczne
-    public SmartAppMemento save(){
+    public SmartAppMemento save() {
         return new SmartAppMemento(this.version);
     }
-    public void load(SmartAppMemento smartAppMemento){
+
+    public void load(SmartAppMemento smartAppMemento) {
         this.version = smartAppMemento.getVersion();
     }
 
